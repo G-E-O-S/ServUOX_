6b6c87d715e7cc1534008d29d31138147f8000b1
Fraz <11639362+Frazurbluu@users.noreply.github.com>
2020-04-04 16:47:41 -0400

    Misc (#38)

--- Scripts/Expansion/Original UO/Items/WristWatch.cs
+++ Scripts/Expansion/Original UO/Items/WristWatch.cs
@@ -1,4 +1,3 @@
-using System;
 
 namespace Server.Items
 {
@@ -8,9 +7,9 @@ namespace Server.Items
         public WristWatch()
             : base(0x1086)
         {
-            this.Weight = this.DefaultWeight;
-            this.LootType = LootType.Blessed;
-            this.Layer = Layer.Bracelet;
+            Weight = this.DefaultWeight;
+            LootType = LootType.Blessed;
+            Layer = Layer.Bracelet;
         }
 
         public WristWatch(Serial serial)
@@ -18,25 +17,14 @@ namespace Server.Items
         {
         }
 
-        public override int LabelNumber
-        {
-            get
-            {
-                return 1041421;
-            }
-        }// a wrist watch
-        public override double DefaultWeight
-        {
-            get
-            {
-                return 1.0;
-            }
-        }
+        public override int LabelNumber { get { return 1041421; } }
+        public override double DefaultWeight { get { return 1.0; } }
+
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
 
-            writer.Write((int)0); // version
+            writer.Write(0); // version
         }
 
         public override void Deserialize(GenericReader reader)
